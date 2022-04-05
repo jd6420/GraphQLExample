b506394447fc11971c141d5e5f04f99b8d338b25
Juan David Cadavid Quintero <juandavid.cadavid@globant.com>
2022-04-05 11:49:24 -0500

    query anidado

--- CoffeeShop/CoffeeShop/Type/MenuType.cs
+++ CoffeeShop/CoffeeShop/Type/MenuType.cs
@@ -1,4 +1,5 @@
-﻿using CoffeeShop.Models;
+﻿using CoffeeShop.Interfaces;
+using CoffeeShop.Models;
 using GraphQL.Types;
 using System;
 using System.Collections.Generic;
@@ -9,11 +10,12 @@ namespace CoffeeShop.Type
 {
     public class MenuType : ObjectGraphType<Menu>
     {
-        public MenuType()
+        public MenuType(ISubmenu submenuService)
         {
             Field(p => p.Id);
             Field(p => p.Name);
             Field(p => p.ImageUrl);
+            Field<ListGraphType<SubmenuType>>("submenus", resolve: context => { return submenuService.GetSubmenus(context.Source.Id); });
         }
     }
 }
